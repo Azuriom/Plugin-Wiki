115cbebee3e5aaca207d30d971e4e70ae2ff1932
Hackmastr <win1dows8@ymail.com>
2022-04-22 21:55:11 +0200

    Add files via upload

---
+++ resources/lang/ca/admin.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    'title' => 'Wiki',
+
+    'categories' => [
+        'title' => 'Categories',
+        'title-edit' => 'Editar categoria :cateogry',
+        'title-create' => 'Crear categoria',
+
+        'create' => 'Afegir categoria',
+        'enable' => 'Habilitar aquesta categoria',
+
+        'empty' => 'No hi ha categories creades.',
+
+        'icons' => 'Pot trobar una llista de icones en <a href="https://fontawesome.com/icons?d=gallery&m=free" target="_blank" rel="noopener noreferrer">FontAwesome</a>.',
+
+        'status' => [
+            'created' => 'S\'ha creat la categoria amb èxit.',
+            'updated' => 'S\'ha modificat la categoria amb èxit.',
+            'deleted' => 'S\'ha esborrat la categoria amb èxit.',
+        ],
+    ],
+
+    'pages' => [
+        'order-updated' => 'Ordre de pàgines actualitzat.',
+    ],
+
+    'permission' => 'Administrar Wiki',
+];
