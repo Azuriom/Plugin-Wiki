8c81c1739b8d74e7833d9cd7889f28d2c9685986
Elikill58 <arpetzouille@gmail.com>
2022-08-01 13:15:35 +0200

    Don't fully refresh page when changing page (#13)

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -9,7 +9,11 @@
         <div class="col-md-3">
             <div class="list-group mb-3" role="tablist">
                 @foreach($page->category->pages as $catPage)
-                    <a href="{{ route('wiki.pages.show', [$page->category, $catPage]) }}" class="list-group-item @if($page->is($catPage)) active @endif">
+                    <a href="{{ route('wiki.pages.show', [$page->category, $catPage]) }}" class="list-group-item @if($page->is($catPage)) active @endif"
+                       title="{{ $catPage->title }}"
+                       onclick="selectWikiPage(this)"
+                       data-bs-toggle="tab" data-bs-target="#page-{{ $catPage->id }}" role="tab"
+                       aria-controls="page-{{ $catPage->id }}" aria-selected="{{ $page->is($catPage) ? 'true' : 'false' }}">
                         {{ $catPage->title }}
                     </a>
                 @endforeach
@@ -20,12 +24,44 @@
             </a>
         </div>
 
-        <div class="col-md-9">
-            <div class="card">
-                <div class="card-body">
-                    {!! $page->content !!}
+        <div class="col-md-9 tab-content">
+            @foreach($page->category->pages as $catPage)
+                <div class="tab-pane fade @if($page->is($catPage)) show active @endif" id="page-{{ $catPage->id }}" role="tabpanel" aria-labelledby="nav-home-tab">
+                    <div class="card">
+                        <div class="card-body">
+                            {!! $catPage->content !!}
+                        </div>
+                    </div>
                 </div>
-            </div>
+            @endforeach
         </div>
     </div>
 @endsection
+
+@push('scripts')
+    <script>
+        let currentTitle = '{{ $page->title }}';
+
+        function selectWikiPage(element, replaceState = false) {
+            const tab = bootstrap.Tab.getOrCreateInstance(element);
+            tab.show();
+
+            if (replaceState) {
+                window.history.replaceState({}, '', element.href);
+            } else {
+                window.history.pushState({}, '', element.href);
+            }
+
+            document.title = document.title.replace(currentTitle, element.title);
+            currentTitle = element.title;
+        }
+
+        window.onpopstate = function(e) {
+            const target = document.querySelector('[href="' + e.target.location.href + '"]');
+
+            if (target) {
+                selectWikiPage(target, true);
+            }
+        };
+    </script>
+@endpush
