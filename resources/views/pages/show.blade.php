e73ce978a04d7cff884152523491c937da102a67
Elikill58 <arpetzouille@gmail.com>
2022-08-01 10:12:08 +0200

    Don't need to refresh full page between multiple page

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -9,7 +9,7 @@
         <div class="col-md-3">
             <div class="list-group mb-3" role="tablist">
                 @foreach($page->category->pages as $catPage)
-                    <a href="{{ route('wiki.pages.show', [$page->category, $catPage]) }}" class="list-group-item @if($page->is($catPage)) active @endif">
+                    <a class="list-group-item @if($page->is($catPage)) active @endif" id="page-list-{{ $catPage->id }}" onclick="select({{ $catPage->id }})">
                         {{ $catPage->title }}
                     </a>
                 @endforeach
@@ -21,11 +21,35 @@
         </div>
 
         <div class="col-md-9">
-            <div class="card">
-                <div class="card-body">
-                    {!! $page->content !!}
+            @foreach($page->category->pages as $catPage)
+                <div class="card card-wiki" id="page-{{ $catPage->id }}" @if($catPage->id != $page->id) style="display: none;" @endif>
+                    <div class="card-body">
+                        {!! $catPage->content !!}
+                    </div>
                 </div>
-            </div>
+            @endforeach
         </div>
     </div>
 @endsection
+
+@push('scripts')
+    <script>
+        var focusPage = {{ $page->id }};
+
+        function select(pageId) {
+            changeIfExist("page-" + focusPage, function (div) { div.style.display = 'none'; });
+            changeIfExist("page-list-" + focusPage, function (div) { div.classList.remove("active"); });
+
+            changeIfExist("page-" + pageId, function (div) { div.style.display = null; });
+            changeIfExist("page-list-" + pageId, function (div) { div.classList.add("active"); });
+            focusPage = pageId;
+        }
+
+        function changeIfExist(name, action) {
+            let div = document.getElementById(name);
+            if(div != null) {
+                action(div);
+            }
+        }
+    </script>
+@endpush
