9457c51efbab5369b300772fa7a53299a6a6c918
jhqwqmc <113768629+jhqwqmc@users.noreply.github.com>
2024-03-07 10:49:11 +0800

    Create admin.php

---
+++ resources/lang/zh-CN/admin.php
@@ -0,0 +1,23 @@
+return [
+    'title' => 'Wiki',
+
+    'categories' => [
+        'title' => '分类',
+        'edit' => '编辑分类 :category',
+        'create' => '创建分类',
+
+        'add' => '添加分类',
+        'enable' => '启用此分类',
+        'parent' => '父分类',
+        'private' => '限制可以访问此分类的角色',
+
+        'empty' => '当前没有创建任何分类。',
+    ],
+
+    'pages' => [
+        'category' => '分类',
+        'updated' => '页面顺序已更新。',
+    ],
+
+    'permission' => '管理维基',
+];
