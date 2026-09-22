6ca41c60536d1e9a049b0453c5012358a613816c
Ente_3 <skypelajos@gmail.com>
2026-09-22 09:29:43 +0200

    Fix repeated pagination links in search results

--- resources/views/pages/search.blade.php
+++ resources/views/pages/search.blade.php
@@ -26,12 +26,12 @@
                     </div>
                 </div>
             @endcan
-
-            {{ $pages->withQueryString()->links() }}
         @empty
             <div class="alert alert-info">
                 <i class="bi bi-info-circle"></i> {{ trans('wiki::messages.search.empty') }}
             </div>
         @endforelse
+
+        {{ $pages->withQueryString()->links() }}
     </div>
 @endsection
